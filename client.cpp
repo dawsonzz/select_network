4a7d067c004a59a9909d2226f180fb6b68ec569e
zhuodongsheng <apple@Dawson.local>
2019-04-11 17:28:59 +0800

    client 多线程

--- client.cpp
+++ client.cpp
@@ -21,57 +21,77 @@ void cmdThread()
     }
 }
 
-int main()
-{
-    //windows中fd_set决定最大数量为63个客户端+1个服务器
-    //mac 和 windows 修改宏定义
-    //linux中最大数量为1024，且写在内核中无法修改
-    const int cCount = 10;
-    //TODO: mac中数量超过252连接失败，客户端二次连接程序报错
+//windows中fd_set决定最大数量为63个客户端+1个服务器
+//mac 和 windows 修改宏定义
+//linux中最大数量为1024，且写在内核中无法修改
+const int cCount = 100;
+//TODO: mac中数量超过252连接失败，客户端二次连接程序报错
 
-    EasyTcpClient* client[cCount];
-    for(int n=0; n<cCount; n++)
+//线程数量
+const int tCount = 4;
+
+EasyTcpClient* client[cCount];
+
+void sendThread(int id) //四个线程 ID1～4
+{
+    int c = (cCount / tCount);
+    int begin = (id - 1) * c;
+    int end = id * c;
+    for(int n=begin; n<end; n++)
     {
-        if(!g_bRun)
-        {
-            return 0;
-        }
         client[n] = new EasyTcpClient();
         // client[n]->Connect("127.0.0.1", 4567);
     }
 
-    for(int n=0; n<cCount; n++)
+    for(int n=begin; n<end; n++)
     {
-        if(!g_bRun)
-        {
-            return 0;
-        }
         client[n]->Connect("127.0.0.1", 4567);
     }
     
     
-    std::thread t1(cmdThread);
-    t1.detach();
-
+    
     Login login;
     strcpy(login.userName, "lyd");
     strcpy(login.passWord, "lyd");
 
     while(g_bRun)
     {
-        for(int n=0; n<cCount; n++)
+        for(int n=begin; n<end; n++)
         {
             client[n]->SendData(&login);
             // client[n]->OnRun();
         }
     }
 
-    for(int n=0; n<cCount; n++)
+    for(int n=begin; n<end; n++)
     {
         client[n]->Close();
     }
 
+   
+}
+
+int main()
+{
+
+
+    std::thread t(cmdThread);
+    t.detach(); 
+
+
+    //启动发送线程
+    for(int n=0; n<tCount; n++)
+    {
+        std::thread t1(sendThread, n+1);
+        t1.detach();
+    }
+
+    while(g_bRun)
+    {
+        sleep(100);
+    }
+
     printf("已退出。\n");
-    // getchar();
+    getchar();
     return 0;
 }
